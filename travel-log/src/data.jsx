50eca1f09015b99924753b5f644cb97066386374
Luchi <oluucodes@gmail.com>
2023-03-11 16:49:12 -0800

    updated alt-text

--- travel-log/src/data.jsx
+++ travel-log/src/data.jsx
@@ -9,7 +9,8 @@ export default [
       startDate : '12 June, 2024',
       endDate : '10 August, 2024',
       description:"Berlin, Germany’s capital, dates to the 13th century. Reminders of the city's turbulent 20th-century history include its Holocaust memorial and the Berlin Wall's graffitied remains. Divided during the Cold War, its 18th-century Brandenburg Gate has become a symbol of reunification. The city's also known for its art scene and modern landmarks like the gold-colored, swoop-roofed Berliner Philharmonie, built in 1963.",
-      imageUrl : '/images/berlin.jpg'  
+      imageUrl : '/images/berlin.jpg', 
+      altText: 'Berlin' 
     },
     {
       id : 2,
@@ -19,7 +20,8 @@ export default [
       startDate : '11 August, 2024',
       endDate : '12 September, 2024',
       description: 'Amsterdam is the Netherlands’ capital, known for its artistic heritage, elaborate canal system and narrow houses with gabled facades, legacies of the city’s 17th-century Golden Age. Its Museum District houses the Van Gogh Museum, works by Rembrandt and Vermeer at the Rijksmuseum, and modern art at the Stedelijk. Cycling is key to the city’s character, and there are numerous bike paths.',
-      imageUrl : '/images/amsterdam.jpg'  
+      imageUrl : '/images/amsterdam.jpg',
+      altText: 'Amsterdam'  
     },
     {
       id : 3,
@@ -29,7 +31,8 @@ export default [
       startDate : '13 September, 2024',
       endDate : '14 October, 2024',
       description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the foreshore of Sydney Harbour, it is widely regarded as one of the world's most famous and distinctive buildings and a masterpiece of 20th-century architecture.",
-      imageUrl : '/src/assets/sydney-australia.jpg'  
+      imageUrl : '/src/assets/sydney-australia.jpg',
+      altText: 'Australia'  
     },
     {
       id : 4,
@@ -39,7 +42,8 @@ export default [
       startDate : '15   October, 2024',
       endDate : '16 November, 2024',
       description: "Reading is a large town on the Thames and Kennet rivers in southern England. It’s known for the annual Reading Festival, an outdoor rock music event. Shops and riverside restaurants dot the town centre. The Reading Museum contains exhibits on the town’s history and displays a Victorian replica of the Bayeux Tapestry. The ruins of the 12th-century Reading Abbey lie beside Forbury Gardens, a Victorian formal garden.",
-      imageUrl : '/src/assets/reading.jpg'  
+      imageUrl : '/src/assets/reading.jpg',
+      altText: 'Reading'  
     },
     {
       id : 5,
@@ -49,7 +53,8 @@ export default [
       startDate : '17 November, 2024',
       endDate : '18 December, 2024',
       description: "The Palace of Holyroodhouse, commonly referred to as Holyrood Palace or Holyroodhouse, is the official residence of the British monarch in Scotland. Located at the bottom of the Royal Mile in Edinburgh, at the opposite end to Edinburgh Castle, Holyroodhouse has served as the principal royal residence in Scotland since the 16th century, and is a setting for state occasions and official entertaining.",
-      imageUrl : '/images/edinburg.jpg'  
+      imageUrl : '/images/edinburg.jpg',
+      altText: 'Edinburg'  
     },
     {
         id : 6,
@@ -59,7 +64,8 @@ export default [
         startDate : '18 Dec, 2024',
         endDate : '20 Jan, 2025',
         description: "Oxford, a city in central southern England, revolves around its prestigious university, established in the 12th century. The architecture of its 38 colleges in the city’s medieval center led poet Matthew Arnold to nickname it the 'City of Dreaming Spires'. University College and Magdalen College are off the High Street, which runs from Carfax Tower (with city views) to the Botanic Garden on the River Cherwell.",
-        imageUrl : '/src/assets/oxford.jpg'  
+        imageUrl : '/src/assets/oxford.jpg',
+        altText: 'Oxford'  
     },
     {
         id : 7,
@@ -69,7 +75,8 @@ export default [
         startDate : '20rd Jan, 2025',
         endDate : '20rd Feb, 2025',
         description: "London, the capital of England and the United Kingdom, is a 21st-century city with history stretching back to Roman times. At its centre stand the imposing Houses of Parliament, the iconic ‘Big Ben’ clock tower and Westminster Abbey, site of British monarch coronations. Across the Thames River, the London Eye observation wheel provides panoramic views of the South Bank cultural complex, and the entire city.",
-        imageUrl : '/images/London.jpg'  
+        imageUrl : '/images/London.jpg',
+        altText: 'London'  
     },
     {
         id : 8,
@@ -79,7 +86,8 @@ export default [
         startDate : '20rd Feb, 2025',
         endDate : '24th March, 2025',
         description: "Belfast is Northern Ireland’s capital. It was the birthplace of the RMS Titanic, which famously struck an iceberg and sunk in 1912. This legacy is recalled in the renovated dockyards' Titanic Quarter, which includes the Titanic Belfast, an aluminium-clad museum reminiscent of a ship’s hull, as well as shipbuilder Harland & Wolff’s Drawing Offices and the Titanic Slipways, which now host open-air concerts.",
-        imageUrl : '/images/belfast.jpg'  
+        imageUrl : '/images/belfast.jpg',
+        altText: 'Belfast'  
     },
     {
         id : 9,
@@ -89,7 +97,8 @@ export default [
         startDate : '24th March, 5',
         endDate : '24th April, 2025',
         description: "Bristol is a city straddling the River Avon in the southwest of England with a prosperous maritime history. Its former city-centre port is now a cultural hub, the Harbourside, where the M Shed museum explores local social and industrial heritage. The harbour's 19th-century warehouses now contain restaurants, shops and cultural institutions such as contemporary art gallery The Arnolfini",
-        imageUrl : '/images/bristol.jpg'  
+        imageUrl : '/images/bristol.jpg',
+        altText: 'Bristol'  
     },
     {
         id : 10,
@@ -99,7 +108,8 @@ export default [
         startDate : '24th April, 2025',
         endDate : '24th May, 2025',
         description: "Cambridge is a city on the River Cam in eastern England, home to the prestigious University of Cambridge, dating to 1209. University colleges include King’s, famed for its choir and towering Gothic chapel, as well as Trinity, founded by Henry VIII, and St John’s, with its 16th-century Great Gate. University museums have exhibits on archaeology and anthropology, polar exploration, the history of science and zoology.",
-        imageUrl : '/images/cambridge.jpg'  
+        imageUrl : '/images/cambridge.jpg',
+        altText: 'Cambridge'  
     },
     {
         id : 11,
@@ -110,7 +120,8 @@ export default [
         startDate : '24th May, 2025',
         endDate : '24th June, 2025',
         description: "Manchester is a major city in the northwest of England with a rich industrial heritage. The Castlefield conservation area’s 18th-century canal system recalls the city’s days as a textile powerhouse, and visitors can trace this history at the interactive Museum of Science & Industry. The revitalised Salford Quays dockyards now house the Daniel Libeskind-designed Imperial War Museum North and the Lowry cultural centre.",
-        imageUrl : '/images/manchester.jpg'  
+        imageUrl : '/images/manchester.jpg',
+        altText: 'Manchester'  
     },
     {
         id : 12,
@@ -120,6 +131,7 @@ export default [
         startDate : '24th June, 2025',
         endDate : '24th July, 2025',
         description: "Leeds is a city in the northern English county of Yorkshire. On the south bank of the River Aire, the Royal Armouries houses the national collection of arms and artillery. Across the river, the redeveloped industrial area around Call Lane is famed for bars and live music venues under converted railway arches. Leeds Kirkgate Market features hundreds of indoor and outdoor stalls.",
-        imageUrl : 'images//leeds.jpg'  
+        imageUrl : 'images//leeds.jpg',
+        altText: 'Leeds'  
     },
 ]
